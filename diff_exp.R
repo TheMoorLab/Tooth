ccd229e48e834e3819cd864a9fea28a5f24bb565
ldvr <31417812+ldvr@users.noreply.github.com>
2020-02-25 16:36:06 +0100

    Add files via upload

--- diff_exp.R
+++ diff_exp.R
@@ -14,10 +14,12 @@ Idents(cellType) 	 	 	 = "celltype.cond"
 cellType_by_cond 		     = FindMarkers(cellType, ident.1 = paste0(cellTypeChar, "_perio"), ident.2 = paste0(cellTypeChar, "_pulp"), verbose = FALSE)
 head(cellType_by_cond, n = 15)
 cellType_by_cond$gene 	 	 = rownames(cellType_by_cond)
+# Take the top 50 highest fold change (up or down)
 top_cellType1				 = top_n(cellType_by_cond[order(cellType_by_cond$p_val_adj),], n = 50, wt = avg_logFC) # overexpressed in ident.1 (perio)
 top_cellType2				 = top_n(cellType_by_cond[order(cellType_by_cond$p_val_adj),], n = -50, wt = avg_logFC) # overexp. in ident.2 (pulp) 
-
-# test = rbind.data.frame(top_cellType1, top_cellType2)
+#  Select only those for which the adjusted p-value is less than 0.01
+top_cellType1 				 = top_cellType1[top_cellType1$p_val_adj<0.01,]
+top_cellType2 				 = top_cellType2[top_cellType2$p_val_adj<0.01,]
 
 g = DoHeatmap(object = cellType, angle = 45, slot = "scale.data", features = c(top_cellType1$gene,top_cellType2$gene), group.by= "condition", group.colors = c("blue", "red") )
 	ggsave(plot = g, filename = file.path("/IMCR_shares/Moorlab/Common/Tooth_project/R_analysis/ldvr_analyses", 
@@ -28,13 +30,13 @@ list(perio_high = top_cellType1, pulp_high = top_cellType2)
 }
 
 # To run:
-# fibro_perio_pulp 		= diff_exp_by_subpop("Fibroblasts")
-# epi_perio_pulp 		= diff_exp_by_subpop("Epithelial")
-# msc_perio_pulp 		= diff_exp_by_subpop("MSC")
-# immu_perio_pulp 		= diff_exp_by_subpop("Immune")
+fibro_perio_pulp 		= diff_exp_by_subpop("Fibroblasts")
+epi_perio_pulp 			= diff_exp_by_subpop("Epithelial")
+msc_perio_pulp 			= diff_exp_by_subpop("MSC")
+immu_perio_pulp 		= diff_exp_by_subpop("Immune")
 
 # After running the above analysis for epithelial, immune, MSCs and fibroblasts, save the results of the differential gene expression for each of the those cases:
-# save(list=c("epi_perio_pulp", "immu_perio_pulp", "msc_perio_pulp", "fibro_perio_pulp"), file=file.path("/IMCR_shares/Moorlab/Common/Tooth_project/R_analysis/ldvr_analyses", "dge.RData"))
+save(list=c("epi_perio_pulp", "immu_perio_pulp", "msc_perio_pulp", "fibro_perio_pulp"), file=file.path("/IMCR_shares/Moorlab/Common/Tooth_project/R_analysis/ldvr_analyses", "dge.RData"))
 
 
 
