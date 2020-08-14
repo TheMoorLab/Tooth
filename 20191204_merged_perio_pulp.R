c43df7cbd85f0b5347f9700667b30e10f46a3f9b
ldvr <31417812+ldvr@users.noreply.github.com>
2020-08-14 11:45:24 +0200

    Update 20191204_merged_perio_pulp.R
    Sequential sample labeling added matching file names used in data GEO upload.

--- 20191204_merged_perio_pulp.R
+++ 20191204_merged_perio_pulp.R
@@ -5,44 +5,39 @@ library(ggplot2)
 library(sctransform)
 library(harmony)
 
-#prior code seurat V2: 20191111_perio_pulp_all.R
-
-
-perio1 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/perio/v2_C92_scRNAseq_4_Perio1")
+perio1 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/perio/Perio1")
 perio1 <- CreateSeuratObject(counts = perio1,   project = "perio1",min.cells = 3,min.features = 200)
 
-perio2 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/perio/v2_C92_scRNAseq_4_Perio2")
+perio2 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/perio/Perio2")
 perio2 <- CreateSeuratObject(counts = perio2,   project = "perio2",min.cells = 3,min.features = 200)
 
-perio3 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/perio/v2_scRNAseq3_Period_WisToothAdultMildCaries")
+perio3 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/perio/Perio3")
 perio3 <- CreateSeuratObject(counts = perio3,   project = "perio3",min.cells = 3,min.features = 200)
 
-perio6 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/perio/v2_scRNAseq_6_Carious_Periodontium")
-perio6 <- CreateSeuratObject(counts = perio6,   project = "perio6",min.cells = 3,min.features = 200)
+perio4 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/perio/Perio4")
+perio4 <- CreateSeuratObject(counts = perio4,   project = "perio4",min.cells = 3,min.features = 200)
 
-perio7 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/perio/v2_4_scRNAseq_WisdomTooth_Upper_NoTotErupt_7_Perio")
-perio7 <- CreateSeuratObject(counts = perio7,   project = "perio7",min.cells = 3,min.features = 200)
-
-
-pulp2 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/pt2_adult_healthy/pulp")
-pulp2 <- CreateSeuratObject(counts  = pulp2,   project = "pulp2",min.features = 200,min.cells = 3)
-
-pulp3 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/sample3_adult_healthy/pulp")
-pulp3 <- CreateSeuratObject(counts = pulp3,   project = "pulp3",min.features = 200,min.cells = 3)
-
-pulp4 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/sample4_adult_healthy/pulp")
-pulp4 <- CreateSeuratObject(counts = pulp4,   project = "pulp4",min.cells = 3,min.features = 200)
-
-pulp13 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/scRNAseq_13_1_AdultPulpHealthy_14_24")
-pulp13 <- CreateSeuratObject(counts = pulp13,   project = "pulp13v3",min.cells = 3,min.features = 200)
-
-pulp14 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/scRNAseq_14_2_AdultPulpHealthy_2_35_45")
-pulp14 <- CreateSeuratObject(counts = pulp14,   project = "pulp14v3",min.cells = 3,min.features = 200)
+perio5 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/perio/Perio5")
+perio5 <- CreateSeuratObject(counts = perio5,   project = "perio5",min.cells = 3,min.features = 200)
 
 
+pulp1 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/pt2_adult_healthy/pulp")
+pulp1 <- CreateSeuratObject(counts  = pulp1,   project = "pulp1", min.features = 200, min.cells = 3)
+
+pulp2 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/sample3_adult_healthy/pulp")
+pulp2 <- CreateSeuratObject(counts = pulp2,   project = "pulp2", min.features = 200, min.cells = 3)
+
+pulp3 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/sample4_adult_healthy/pulp")
+pulp3 <- CreateSeuratObject(counts = pulp3,   project = "pulp3", min.cells = 3, min.features = 200)
+
+pulp4 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/scRNAseq_13_1_AdultPulpHealthy_14_24")
+pulp4 <- CreateSeuratObject(counts = pulp4 ,   project = "pulp4", min.cells = 3, min.features = 200)
+
+pulp5 <- Read10X(data.dir = "/IMCR_shares/Moorlab/Common/Tooth_project/CellRanger_output/scRNAseq_14_2_AdultPulpHealthy_2_35_45")
+pulp5 <- CreateSeuratObject(counts = pulp5,   project = "pulp5", min.cells = 3, min.features = 200)
 
 
-merged<-merge(x=perio1, y=c(perio2,perio3,perio6,perio7,pulp2,pulp3,pulp4,pulp13,pulp14),add.cell.ids=c("perio1","perio2","perio3","perio6","perio7","pulp2","pulp3","pulp4","pulp13","pulp14"))
+merged<-merge(x=perio1, y=c(perio2,perio3,perio4,perio5,pulp1,pulp2,pulp3,pulp4,pulp5),add.cell.ids=c("perio1","perio2","perio3","perio4","perio5","pulp1","pulp2","pulp3","pulp4","pulp5"))
 
 table(merged$orig.ident)
 
@@ -97,9 +92,6 @@ FeaturePlot(merged_harmony,features=c("percent.mt"), reduction = "umap",order = 
 
 all_markers<-FindAllMarkers(merged_harmony)
 
-#currently markers not run yet
-save(merged_harmony,file = "/IMCR_shares/Moorlab/Common/Tooth_project/R_analysis/r_output/merged_harmony_markers_20191204.rds")
-
 save(merged_harmony,all_markers,file = "/IMCR_shares/Moorlab/Common/Tooth_project/R_analysis/r_output/merged_harmony_markers_20191204.rds")
-save.image(file = "perio_20191204.Rdata")
+
 
